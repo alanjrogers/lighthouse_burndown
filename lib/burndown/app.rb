b7bd591285aa14b7dc78789b2dee9160294ee088
Gabor Ratky <gabor@secretsaucepartners.com>
2010-09-07 10:54:16 +0200

    Uses Sinatra::Base (1.0) instead of Sinatra::Default

--- lib/burndown/app.rb
+++ lib/burndown/app.rb
@@ -1,17 +1,17 @@
 module Burndown
-  class App < Sinatra::Default
+  class App < Sinatra::Base
     set :root,     File.dirname(__FILE__) + "/../.."
     set :app_file, __FILE__
     enable :sessions
 
     include Burndown
     include Burndown::Helpers
-    
+
     get "/" do
       @projects = Project.all
       show :index
     end
-    
+
     get "/project/:id" do
       @project = Project.get(params[:id])
       @milestones = @project.milestones.all(:order => [:due_on.asc]).sort { |x, y|
@@ -24,28 +24,28 @@ module Burndown
       }
       show :project
     end
-    
+
     get "/timeline/:id" do
       @milestone = Milestone.get(params[:id])
       @timeline_events = @milestone.milestone_events(:order => [:created_on.desc])
       show :timeline
     end
-    
+
     get "/setup" do
       @tokens = Token.all
       show :setup
     end
-    
+
     # Validates a token (AJAX method)
     post "/token_validity" do
       @token = Token.new(params[:token])
       status (@token.valid_lighthouse_token? ? 200 : 500)
     end
-    
+
     # Creates a new token (AJAX method)
     post "/tokens" do
       return if Burndown.config[:demo_mode]
-      
+
       @token = Token.new(params[:token])
       @token.set_data
       if @token.save
@@ -54,14 +54,14 @@ module Burndown
         status 500
       end
     end
-    
+
     post "/projects" do
       return if Burndown.config[:demo_mode]
-      
+
       token = Token.get(params[:token_id])
       Project.activate_remote(params[:project_remote_id], token, request.host)
       redirect "/setup"
     end
-    
+
   end
 end
